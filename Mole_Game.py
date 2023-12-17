0735d772fd7c2580c4fdfeb5f3ab4ea3513da002
minchan30 <goalie0901@gmail.com>
2023-12-17 12:26:06 +0900

    :memo:update

--- Mole_Game.py
+++ Mole_Game.py
@@ -2,23 +2,30 @@ import pygame as pg
 
 pg.init()
 
-screen = pg.display.set_mode((600,600))
-pg.display.set_caption("나만의 캐릭터 움직이기 게임")
+screen = pg.display.set_mode((800, 600))
 
-running = True
+pg.display.set_caption("두더지 게임")
+font = pg.font.SysFont('numberuungothic', 30)
 
+score = 0
 
 background_img = pg.image.load('dooduji.webp')
-background_img = pg.transform.scale(background_img, (600, 600))
-character1_img = pg.image.load('doduji.png')
+background_img = pg.transform.scale(background_img, (800, 600))
+character1_img = pg.image.load('doduji-removebg-preview.png')
 character1_img = pg.transform.scale(character1_img, (75, 100))
-character2_img = pg.image.load('hamor.webp')
-character2_img = pg.transform.scale(character2_img, (75, 100))
 
 character1_img_pos = character1_img.get_rect()
-character2_img_pos = character2_img.get_rect()
 
+running = True
 while running:
+
+    time_text = font.render(str(pg.time.get_ticks() // 1000) + ' 초', True, (0, 0, 0), None)
+    screen.blit(time_text, (680, 55))
+    pg.display.update()
+    score_text = font.render(str(score) + '점', True, (0, 0, 0), None)
+    screen.blit(score_text, (55, 55))
+    pg.display.update()
+
     for event in pg.event.get():
         if event.type == pg.QUIT:
             running = False
@@ -36,7 +43,8 @@ while running:
     
     screen.blit(background_img,background_img.get_rect())
     screen.blit(character1_img, character1_img_pos)
-    screen.blit(character2_img, character2_img_pos)
+    screen.blit(time_text, (680, 55))
+    screen.blit(score_text, (55, 55))
 
     pg.display.update()
 
