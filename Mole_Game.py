3e5c7039cd4000f42ccab07e5510da695c0016d9
minchan30 <goalie0901@gmail.com>
2023-12-23 15:45:42 +0900

    :tada:두더지 게임

--- Mole_Game.py
+++ Mole_Game.py
@@ -19,6 +19,8 @@ score = 0
 character1_img_pos = [(70, 100), (335, 100), (595, 100), (200, 240), (465, 240), 
                       (70, 380), (335, 380), (595, 380)]
 
+check_time = True
+
 while running :
 
     screen.blit(background_img,background_img.get_rect())
@@ -42,11 +44,21 @@ while running :
         #         character1_img_pos.y -= 5
         #     elif event.key == pg.K_DOWN :
         #         character1_img_pos.y += 5
-        if event.type == pg.MOUSEBUTTONDOWN :
-            screen.blit(character1_img, random.choice(character1_img_pos))
+        # if event.type == pg.MOUSEBUTTONDOWN :
+        #     screen.blit(character1_img, random.choice(character1_img_pos))
+    if(pg.time.get_ticks() // 1000) % 2 == 0 :
         
+        if check_time == True :
+            check_time = False
+            screen .blit(character1_img, random.choice(character1_img_pos))
             pg.display.update()
-            pg.time.wait(2000)
+            pg.time.wait(500)
+    else:
+        check_time = True
+            
+        
+            # pg.display.update()
+            # pg.time.wait(2000)
 
                 
     #screen.blit(character1_img, character1_img_pos)
